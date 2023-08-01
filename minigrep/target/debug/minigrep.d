950ed15e4c2e37d0c959b4ad5059296da8ecc845
Wicho <54341712+Wichosu@users.noreply.github.com>
2023-08-01 13:09:07 -0600

    Minigrep done

--- minigrep/target/debug/minigrep.d
+++ minigrep/target/debug/minigrep.d
@@ -1,1 +1,1 @@
-/home/wicho/Desktop/projects/minigrep/target/debug/minigrep: /home/wicho/Desktop/projects/minigrep/src/main.rs
+/home/wicho/Desktop/projects/minigrep/target/debug/minigrep: /home/wicho/Desktop/projects/minigrep/src/lib.rs /home/wicho/Desktop/projects/minigrep/src/main.rs
